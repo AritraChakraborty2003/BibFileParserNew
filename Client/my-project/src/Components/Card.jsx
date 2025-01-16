bb48000a6642074cdfd15041f2e50557735ed701
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2025-01-16 23:49:28 +0530

    Update Card.jsx

--- Client/my-project/src/Components/Card.jsx
+++ Client/my-project/src/Components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <>
       {props.data.map((val) => (
-        <div className="holder w-[70%] border-[1px] p-[4vmin]">
+        <div className="holder w-[98%] lg:w-[70%] border-[1px] p-[4vmin]">
           <p className="font-bold">Title: {val.entryTags.TITLE}</p>
           <p>Author: {val.entryTags.AUTHOR}</p>
           <p>year:{val.entryTags.YEAR}</p>
