685a7ab643cc683c27734861d6f03ea812b15ed6
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2025-01-16 23:24:58 +0530

    Update verifyUser.js

--- Backend/Middlewares/verifyUser.js
+++ Backend/Middlewares/verifyUser.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export const verifyUser = (req, res, next) => {
   //   console.log(req.query, process.env.apiKey);
-  if (req.query.apiKey === process.env.apiKey) {
+  // if (req.query.apiKey === process.env.apiKey) {
     next();
-  } else {
-    res.json({ status: 401, message: "Invalid API key" });
-  }
+  // } else {
+  //   res.json({ status: 401, message: "Invalid API key" });
+  // }
 };
